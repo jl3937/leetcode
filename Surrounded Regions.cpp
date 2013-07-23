3e587011df6179cc6a1ce404945ecaff42d3328e
Junyang Lu <junyangl@google.com>
2013-07-23 14:37:06 -0700

    correct surrounded regions.

--- Surrounded Regions.cpp
+++ Surrounded Regions.cpp
@@ -1,8 +1,8 @@
 int x[] = {0, 0, -1, 1};
 int y[] = {-1, 1, 0, 0};
 class Solution {
-public:
-  void solve(vector<vector<char>> &board) {
+ public:
+  void solve(vector<vector<char> >& board) {
     // Start typing your C/C++ solution below
     // DO NOT write int main() function
     m = board.size();
@@ -26,30 +26,57 @@ public:
       }
     }
   }
-private:
+
+ private:
   int m;
   int n;
-  bool reachEdge(vector<vector<char>> &board, int i, int j) {
-    if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != 'O')
+  bool reachEdge(vector<vector<char> >& board, int i, int j) {
+    if (board[i][j] != 'O')
       return false;
-    board[i][j] = '.';
+    queue<pair<int, int> > q;
     bool reach = false;
-    for (int d = 0; d < 4; d++) {
-      if (reachEdge(board, i + x[d], j + y[d])) {
-        reach = true;
+
+    board[i][j] = '.';
+    q.push(make_pair(i, j));
+    if (i == 0 || i == m - 1 || j == 0 || j == n - 1)
+      reach = true;
+    while (!q.empty()) {
+      pair<int, int> pos = q.front();
+      i = pos.first;
+      j = pos.second;
+      q.pop();
+      for (int d = 0; d < 4; d++) {
+        if (i + x[d] >= 0 && i + x[d] < m && j + y[d] >= 0 && j + y[d] < n &&
+            board[i + x[d]][j + y[d]] == 'O') {
+          board[i + x[d]][j + y[d]] = '.';
+          if (i + x[d] == 0 || i + x[d] == m - 1 || j + y[d] == 0 ||
+              j + y[d] == n - 1)
+            reach = true;
+          q.push(make_pair(i + x[d], j + y[d]));
+        }
       }
     }
-    if (i == 0 || i == m - 1 || j == 0 || j == n - 1)
-      reach = true;;
     return reach;
   }
-  
-  void flip(vector<vector<char>> &board, int i, int j) {
-    if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != '.')
-      return;
+
+  void flip(vector<vector<char> >& board, int i, int j) {
+    queue<pair<int, int> > q;
+    bool reach = false;
+
     board[i][j] = 'X';
-    for (int d = 0; d < 4; d++) {
-      flip(board, i + x[d], j + y[d]);
+    q.push(make_pair(i, j));
+    while (!q.empty()) {
+      pair<int, int> pos = q.front();
+      i = pos.first;
+      j = pos.second;
+      q.pop();
+      for (int d = 0; d < 4; d++) {
+        if (i + x[d] >= 0 && i + x[d] < m && j + y[d] >= 0 && j + y[d] < n &&
+            board[i + x[d]][j + y[d]] == '.') {
+          board[i + x[d]][j + y[d]] = 'X';
+          q.push(make_pair(i + x[d], j + y[d]));
+        }
+      }
     }
   }
 };
