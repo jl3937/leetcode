81b59702f4b874a084f094f60f9737353509fd49
Junyang Lu <junyangl@google.com>
2013-07-24 10:13:29 -0700

    passed longest valid parenthesis.

--- LongestValidParentheses.cpp
+++ LongestValidParentheses.cpp
@@ -11,20 +11,38 @@ class Solution {
     while (i < n) {
       if (s[i] == '(') {
         left++;
-        i++;
       } else if (left) {
         left--;
         if (left == 0) {
           if (i - start + 1 > max)
             max = i - start + 1;
         }
-        i++;
       } else {
-        while (i < n && s[i] == ')') {
-          i++;
-        }
-        start = i;
+        start = i + 1;
       }
+      i++;
+    }
+    if (!left)
+      return max;
+
+    i = n - 1;
+    int end = start;
+    start = i;
+    int right = 0;
+    while (i >= end) {
+      if (s[i] == ')')
+        right++;
+      else if (right) {
+        right--;
+        if (right == 0) {
+          if (start - i + 1 > max) {
+            max = start - i + 1;
+          }
+        }
+      } else {
+        start = i - 1;
+      }
+      i--;
     }
     return max;
   }
