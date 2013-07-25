2471a0a7751992b00f5674413d7e6bb0605e952f
Junyang Lu <junyangl@google.com>
2013-07-24 18:45:03 -0700

    add new file.

---
+++ LargestRectangleInHistogram.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int> &height) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        stack<int> s; // indexes for increasing bars
+        int max = 0;
+        height.push_back(0);
+        for (int i = 0; i < height.size();) {
+          if (s.empty() || height[i] >= height[s.top()]) {
+            s.push(i);
+            i++;
+          } else {
+            int start = s.top();
+            s.pop();
+            int area = (s.empty() ? i : (i - start)) * height[start];
+            if (area > max)
+              max = area;
+          }
+        }
+
+        return max;
+    }
+};
