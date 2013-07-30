55bc82f7bf90a56e8047480cc90a377c7188db6a
卢 俊旸 <lujunyang@ludeMacBook-Pro-4.local>
2013-07-29 23:14:05 -0700

    edit two files.

--- ImplementStrStr.cpp
+++ ImplementStrStr.cpp
@@ -3,14 +3,26 @@ public:
   char *strStr(char *haystack, char *needle) {
     // Start typing your C/C++ solution below
     // DO NOT write int main() function
-    for (int i = 0; i == 0 || haystack[i] != '\0'; i++) {
-      int j;
-      for (j = 0; needle[j] != '\0'; j++) {
-        if (haystack[i + j] != needle[j])
-          break;
+    if (needle[0] == 0)
+        return haystack;
+    int p[40000];
+    p[0] = -1;
+    for (int i = 0; needle[i] != '\0'; i++) {
+      int j = p[i];
+      while (j != -1 && needle[i] != needle[j]) {
+        j = p[j];
       }
-      if (needle[j] == '\0')
+      p[i + 1] = j + 1;
+    }
+    int i = 0, j = 0;
+    while (i == 0 || haystack[i + j] != '\0') {
+      while (j != -1 && haystack[i + j] != needle[j]) {
+        i += j - p[j];
+        j = p[j];
+      }
+      if (needle[j + 1] == '\0')
         return haystack + i;
+      j++;
     }
     return NULL;
   }
--- LargestRectangleInHistogram.cpp
+++ LargestRectangleInHistogram.cpp
@@ -1,22 +1,20 @@
 class Solution {
- public:
-  int largestRectangleArea(vector<int>& height) {
+public:
+  int largestRectangleArea(vector<int> &height) {
     // Start typing your C/C++ solution below
     // DO NOT write int main() function
-    stack<int> s;  // indexes for increasing bars
+    stack<int> s; // indexes for increasing bars
     int max = 0;
     height.push_back(0);
-    for (int i = 0; i < height.size();) {
-      if (s.empty() || height[i] >= height[s.top()]) {
-        s.push(i);
-        i++;
-      } else {
+    for (int i = 0; i < height.size(); i++) {
+      while (!s.empty() && height[s.top()] > height[i]) {
         int start = s.top();
         s.pop();
         int area = (s.empty() ? i : (i - s.top() - 1)) * height[start];
         if (area > max)
           max = area;
       }
+      s.push(i);
     }
     return max;
   }
