354bd01e58963c3a7558fd4fd124441fecb8755a
卢 俊旸 <lujunyang@ludeMacBook-Pro-4.local>
2013-09-02 17:17:55 -0400

    modify palindrome 2

--- PalindromePartitionII.cpp
+++ PalindromePartitionII.cpp
@@ -1,34 +1,30 @@
+// 2-step dynamic programming
+// s[i...j] is palindrome if s[i+1...j-1] is palindrome and s[i] == s[j]
+bool p[2048][2048];
+int cut[2048];
 class Solution {
- public:
+public:
   int minCut(string s) {
     // Start typing your C/C++ solution below
     // DO NOT write int main() function
-    vector<int> cut;
-    cut.resize(s.size() + 1);
+    int len = s.size();
+    for (int i = len - 1; i >= 0; i--) {
+      for (int j = i; j < len; j++) {
+        p[i][j] = s[i] == s[j] && (i + 1 >= j - 1 || p[i + 1][j - 1]);
+      }
+    }
     cut[0] = -1;
-    for (int end = 0; end < s.size(); end++) {
-      int min = INT_MAX;
-      for (int start = end; start >= 0; start--) {
-        if (cut[start] + 1 >= min)
-          continue;
-        int i = start, j = end;
-        bool palindrome = true;
-        while (i < j) {
-          if (s[i] != s[j]) {
-            palindrome = false;
-            break;
-          }
-          i++;
-          j--;
-        }
-        if (palindrome) {
-          min = cut[start] + 1;
-          if (min == 0)
+    for (int i = 0; i < len; i++) {
+      int min_cut = i;
+      for (int j = 0; j <= i; j++) {
+        if (p[j][i]) {
+          min_cut = min(min_cut, cut[j] + 1);
+          if (min_cut <= 1)
             break;
         }
       }
-      cut[end + 1] = min;
+      cut[i + 1] = min_cut;
     }
-    return cut[s.size()];
+    return cut[len];
   }
 };
