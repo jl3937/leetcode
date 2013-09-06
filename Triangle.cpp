97f6bd1526a229826d183e4102966925929f8629
卢 俊旸 <lujunyang@ludeMacBook-Pro-4.local>
2013-09-05 23:21:30 -0400

    modify file.

--- Triangle.cpp
+++ Triangle.cpp
@@ -1,30 +1,17 @@
 class Solution {
- public:
-  // element in triangle may be negative!!
-  int minimumTotal(vector<vector<int> >& triangle) {
-    int total[512];
-    int old_total[512];
-    total[0] = triangle[0][0];
+public:
+  int minimumTotal(vector<vector<int> > &triangle) {
+    // Start typing your C/C++ solution below
+    // DO NOT write int main() function
+    if (triangle.empty())
+      return 0;
     int n = triangle.size();
-    for (int i = 1; i < n; i++) {
-      memcpy(old_total, total, sizeof(total));
+    vector<int> total(triangle[n - 1]);
+    for (int i = n - 2; i >= 0; i--) {
       for (int j = 0; j <= i; j++) {
-        if (j == 0) {
-          total[0] = old_total[0] + triangle[i][0];
-        } else if (j == i) {
-          total[j] = old_total[j - 1] + triangle[i][j];
-        } else {
-          total[j] = min(old_total[j - 1], old_total[j]) + triangle[i][j];
-        }
+        total[j] = triangle[i][j] + min(total[j], total[j + 1]);
       }
     }
-
-    int min = total[0];
-    for (int i = 1; i < n; i++) {
-      if (total[i] < min) {
-        min = total[i];
-      }
-    }
-    return min;
+    return total[0];
   }
 };
