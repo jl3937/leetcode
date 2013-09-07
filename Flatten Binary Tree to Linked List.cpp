627609ca56f7ffb5e537aeb502dfbc11862f2508
卢 俊旸 <lujunyang@dyn-160-39-173-109.dyn.columbia.edu>
2013-09-07 16:21:12 -0400

    modify file

--- Flatten Binary Tree to Linked List.cpp
+++ Flatten Binary Tree to Linked List.cpp
@@ -1,46 +1,33 @@
-#include <iostream>
-
-using namespace std;
-
-struct TreeNode {
-  int val;
-  TreeNode* left;
-  TreeNode* right;
-  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
- public:
-  void flatten(TreeNode* root) {
+public:
+  void flatten(TreeNode *root) {
     // Start typing your C/C++ solution below
     // DO NOT write int main() function
-    if (!root)
-      return;
     flattenWithTail(root);
   }
-
- private:
-  // Return tail node
-  TreeNode* flattenWithTail(TreeNode* root) {
-    TreeNode* left = root->left;
-    TreeNode* right = root->right;
-    root->left = NULL; // Don't forget to set left child NULL.
-    TreeNode* cur = root;
+  TreeNode *flattenWithTail(TreeNode *root) {
+    if (!root)
+      return NULL;
+    TreeNode *left = flattenWithTail(root->left);
+    TreeNode *right = flattenWithTail(root->right);
     if (left) {
-      cur->right = left;
-      cur = flattenWithTail(left);
+      left->right = root->right;
+      root->right = root->left;
+      root->left = NULL; // Don't forget to set left child NULL.
     }
-    if (right) {
-      cur->right = right;
-      cur = flattenWithTail(right);
-    }
-    return cur;
+    if (right)
+      return right;
+    if (left)
+      return left;
+    return root;
   }
 };
-
-int main() {
-  TreeNode* root = new TreeNode(1);
-  root->right = new TreeNode(2);
-  Solution s;
-  s.flatten(root);
-}
